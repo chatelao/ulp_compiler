56f103cdbb5bb3e37b89fe00d18c482e1071bdd3
chatelao <olivier.chatelain@gmail.com>
2019-01-27 20:38:09 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 set path=D:\Dropbox\Privat\2019-01-27_ULP_Complier\esp32ulp-elf-binutils\bin
 
-REM esp32ulp-elf-as.exe -Wa -al -g test.S
-REM esp32ulp-elf-as.exe -al -g test2.S
-esp32ulp-elf-as.exe -acdhlns test2.S
+esp32ulp-elf-as.exe -acdhlns test2.S > test2.lst
